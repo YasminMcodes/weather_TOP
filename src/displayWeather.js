440e70860502fce4c0ad067f504edf34b0ecb831
YasminMcodes <jessie.mahmud@gmail.com>
2026-01-13 12:10:32 +0100

    added a temperature unit toggle button with hover  effect and adjusted weather card layout for better  positioning of the toggle button.

--- src/displayWeather.js
+++ src/displayWeather.js
@@ -14,22 +14,43 @@ export function displayWeather(data, errorMessage = null) {
   errorDiv.style.display = 'none';
   container.innerHTML = `
   <div class="weather-card">
+  <button id = "toggleTemp">°F</button>
     <div class="weather-header"> 
-  <h2 class = 'city'> ${data.city} </h2>;
+  <h2 class = 'city'> ${data.city}</h2>  
   <img class = "weather-icon" src="https://raw.githubusercontent.com/VisualCrossing/WeatherIcons/main/PNG/1st%20Set%20-%20Color/${data.icon}.png" alt="${data.description}"/>
   </div>
   <div class="weather-main">
-  <p class = 'temperature'> 🌡️Temperature: ${data.temperature}°C </p>
-  <p class = 'feelslike'> 🤗Feels Like: ${data.feelslike}°C </p></div
-  <p class = 'description'> 🌥️Conditions: ${data.description} </p>
+  <p class = 'temperature'>  🌡️ Temperature: <span id="temp">${data.temperature}°C</span></p>
+  <p class = 'feelslike'> 🤗 Feels Like: <span id="feelslike">${data.feelslike}°C </span> </p>
+  </div>
+  <p class = 'description'> 🌥️ Conditions: ${data.description} </p>
   </div>
   <div class="weather-details">
   <div class = "detail">
-  <p class = 'humidity'> <span> 💧Humidity: </span> <span>${data.humidity}%</span> </p>
+  <p class = 'humidity'> <span> 💧 Humidity: </span> <span>${data.humidity}%</span> </p>
   </div>
     <div class = "detail">
-  <p class = 'windspeed'> <span> 🌬️Windspeed: </span> <span>${data.windspeed} km/h</span> </p>
+  <p class = 'windspeed'> <span> 🌬️ Windspeed: </span> <span>${data.windspeed} km/h</span> </p>
   </div>
   </div>
   </div>`;
+  const tempEl = document.getElementById('temp');
+  const feelslikeEl = document.getElementById('feelslike');
+  const toggleBtn = document.getElementById('toggleTemp');
+
+  let iscelsius = true;
+  toggleBtn.addEventListener('click', () => {
+    if (iscelsius) {
+      tempEl.textContent = ((data.temperature * 9) / 5 + 32).toFixed(2) + '°F';
+      feelslikeEl.textContent =
+        ((data.feelslike * 9) / 5 + 32).toFixed(2) + '°F';
+      toggleBtn.textContent = '°C';
+      iscelsius = false;
+    } else {
+      tempEl.textContent = data.temperature + '°C';
+      feelslikeEl.textContent = data.feelslike + '°C';
+      toggleBtn.textContent = '°F';
+      iscelsius = true;
+    }
+  });
 }
